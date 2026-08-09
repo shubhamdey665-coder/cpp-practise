0b1f64d4542ab814779126f334daa859abb15667
shubhamdey665-coder <shubhamdey665@gmail.com>
2026-08-09 14:10:29 +0530

    Initial commit

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int n=6;
+    for(int i=1;i<=n;i++)
+    {
+        char ch='A';
+        for(int j=1;j<=n;j++)
+        {
+            cout<<ch;
+            ch++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
