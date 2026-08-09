543dcc2563931cef7710878e94b711976983082e
shubhamdey665-coder <shubhamdey665@gmail.com>
2026-08-09 15:38:00 +0530

    9th aug 26

--- code.cpp
+++ code.cpp
@@ -2,15 +2,22 @@
 using namespace std ;
 int main()
 {
-    int n=6;
+    int n=4;
+    
+
     for(int i=1;i<=n;i++)
     {
-        char ch='A';
-        for(int j=1;j<=n;j++)
+       for(int k=0;k<i;k++)
         {
-            cout<<ch;
-            ch++;
+            cout<<" ";
         }
+        for(int j=n;j>=i;j--)
+        {
+            cout<<i;
+            
+            
+        }
+        
         cout<<endl;
     }
     return 0;
